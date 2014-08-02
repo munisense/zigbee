b9b194940afc0a6d430965e72e482742a07e33ac
Joost Pastoor <jopast@gmail.com>
2014-08-02 21:05:20 +0200

    Work in progress on the Node Descriptor

---
+++ tests/ZDP/Discovery/NodeDescriptorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Munisense\Zigbee\ZDP\Discovery;
+
+class NodeDescriptorTest extends \PHPUnit_Framework_TestCase
+  {
+  /**
+   * TODO Write tests
+   */
+   public function testTest()
+     {
+
+     }
+  }
+ 
