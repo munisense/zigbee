18d82c01548c5b8b62139304b71cc75f054488f5
Frank Versnel <f.versnel@gmail.com>
2014-08-08 14:45:20 +0200

    Start index is no longer optional in IEEE address request and NWK address request.
    See 2.4.3.1.2 and 2.4.3.1.1 of the Zigbee spec.

--- src/ZDP/Discovery/NwkAddrReqCommand.php
+++ src/ZDP/Discovery/NwkAddrReqCommand.php
@@ -31,6 +31,7 @@ class NwkAddrReqCommand extends AbstractFrame implements IZDPCommandFrame
     $frame = new self;
     $frame->setRequestType(self::REQUEST_TYPE_SINGLE);
     $frame->setIeeeAddress($ieee_address);
+    $frame->setStartIndex(0x00);
     return $frame;
     }
 
@@ -47,8 +48,7 @@ class NwkAddrReqCommand extends AbstractFrame implements IZDPCommandFrame
     {
     $this->setIeeeAddress(Buffer::unpackEui64($frame));
     $this->setRequestType(Buffer::unpackInt8u($frame));
-    if($this->isStartIndexPresent())
-      $this->setStartIndex(Buffer::unpackInt8u($frame));
+    $this->setStartIndex(Buffer::unpackInt8u($frame));
     }
 
   public function getFrame()
@@ -57,8 +57,7 @@ class NwkAddrReqCommand extends AbstractFrame implements IZDPCommandFrame
 
     Buffer::packEui64($frame, $this->getIeeeAddress());
     Buffer::packInt8u($frame, $this->getRequestType());
-    if($this->isStartIndexPresent())
-      Buffer::packInt8u($frame, $this->getStartIndex());
+    Buffer::packInt8u($frame, $this->getStartIndex());
 
     return $frame;
     }
@@ -126,21 +125,12 @@ class NwkAddrReqCommand extends AbstractFrame implements IZDPCommandFrame
     return sprintf("0x%02x", $this->getStartIndex());
     }
 
-  private function isStartIndexPresent()
-    {
-    if($this->getRequestType() === self::REQUEST_TYPE_EXTENDED)
-      return true;
-
-    return false;
-    }
-
   public function __toString()
     {
     $output = __CLASS__." (length: ".strlen($this->getFrame()).")".PHP_EOL;
     $output .= "|- IeeeAddr    : ".$this->displayIeeeAddress().PHP_EOL;
-    $output .= ($this->isStartIndexPresent() ? "|" : "`")."- RequestType : ".$this->displayRequestType().PHP_EOL;
-    if($this->isStartIndexPresent())
-      $output .= "`- StartIndex  : ".$this->displayStartIndex().PHP_EOL;
+    $output .= "|- RequestType : ".$this->displayRequestType().PHP_EOL;
+    $output .= "`- StartIndex  : ".$this->displayStartIndex().PHP_EOL;
 
     return $output;
     }
